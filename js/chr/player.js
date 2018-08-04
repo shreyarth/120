6cb27be658cb09a56f6d34799deb24496a4977ce
100sung <38180151+100sung@users.noreply.github.com>
2018-08-03 19:33:02 -0700

    Update player.js

--- js/chr/player.js
+++ js/chr/player.js
@@ -2,7 +2,19 @@
 function Player(game, key, frame) {
 	// Phaser.Sprite(game, x, y, key)
 	// game.rnd.integerInRange(min, max) returns rand int between min, max
-	Phaser.Sprite.call(this, game, 64, 110, key, frame);
+	//Phaser.Sprite.call(this, game, 64, 110, key, frame);
+	//Phaser.Sprite.call(this, game, 470, 500, 0,'player', 33);
+	
+	Phaser.Sprite(this, game, 300, 300, 'player');
+	this.scale.x = 3;
+	this.scale.y = 3;
+	// physics crap
+	game.physics.arcade.enable(player);
+	player.body.bounce.y = 0.2;
+	player.body.gravity.y = 500;
+	player.body.collideWorldBounds = true;
+
+
 	
 	// anchor: Origin of the texture
 	// 0.5 = center
@@ -10,6 +22,7 @@ function Player(game, key, frame) {
 
 	// Character info
 	this.health = 100;
+	this.pooCount = 100;
 
 	game.physics.enable(this);
 }
@@ -21,14 +34,20 @@ Player.prototype.constructor = Player;
 // override Phaser.Sprite update
 Player.prototype.update = function() {
 	// Controls
-	if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
+	if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
 		this.body.velocity.x -= 5;
-	if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
+		console.log("left");
+	}
+	if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
 		this.body.velocity.x += 5;
+		console.log("right");
+	}
 
 	// Jump (need double jump)
-	if(game.input.keyboard.justPressed(Phaser.Keyboard.UP))
+	if(game.input.keyboard.justPressed(Phaser.Keyboard.UP)){
 		this.body.velocity.y -= 200;
+		console.log("jump");
+	}
 
 	// Attack move
 }
