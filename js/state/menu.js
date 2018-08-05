418fe262e439aa47134af232a079547b85e8a9c4
100sung <38180151+100sung@users.noreply.github.com>
2018-08-05 15:53:43 -0700

    Add files via upload

--- js/state/menu.js
+++ js/state/menu.js
@@ -5,15 +5,25 @@ var menu = function() {
 menu.prototype = {
 	preload: function() {
 		game.load.path = 'assets/';
+		// images
 		game.load.image('player', 'img/player.png');
 		game.load.image('poo', 'img/star.png');
 		game.load.image('platform', 'img/platform.png');
 		game.load.image('star', 'img/star.png');
+		game.load.image('enemy', 'img/enemy.png');
+
+		//audio
+		game.load.audio('fart', 'audio/fart.mp3');
+		game.load.audio('fartdead', 'audio/Rasp.mp3');
 		// Call menu assets
 	},
 	create: function() {
 		// Asset implementaion
+		game.stage.backgroundColor = "#895f2b";
 		console.log("menu state to check implementation");
+		var playText = game.add.text(250, game.height*.3, 
+				'    To shit or not to shit...\nPush spacebar to continue', 
+				{font: 'Helvetica', fontSize: '24px', fill: '#fff'});
 	},
 	update: function() {
 		// Update function
--- js/state/play.js
+++ js/state/play.js
@@ -1,25 +1,43 @@
 var play = function() {
 	// Global state variables
-	var platform;
+	this.bullets, this.enemy;
+	this.platform;
 }
 function death(){
 	if(pooCount < 0){
+		// deathpoo = game.add.audio('fartdead', 1);
+		// deathpoo.play();
 		player.kill();
 		console.log("death from no poo");
 		player.reset(300,300);
 		pooCount = 10;
-	}
-	if(pooCount > 100){
+		
+	}else
+		// deathpoo = game.add.audio('fartdead', 0.6);
+		deathpoo.allowMultiple = false;
+		deathpoo.play();
 		player.kill();
+		player.reset(-200,300);
 		console.log("death from too much poo");
-		player.reset(300,300);
-		pooCount = 90;
+		// var playText = game.add.text(game.width/2, game.height*.3, 
+		// 		'You have just Shat yourself\nPush Enter to respawn', 
+		// 		{font: 'Helvetica', fontSize: '24px', fill: '#fff'});
+		if(game.input.keyboard.isDown(Phaser.Keyboard.ENTER)){
+			player.reset(300,300);
+			pooCount = 90;
+			
+		
 	}
 };
 
 play.prototype = {
 	preload: function() {
-		// Call menu assets
+		game.load.path = 'assets/';
+		game.load.image('player', 'img/player.png');
+		game.load.image('poo', 'img/star.png');
+		game.load.image('platform', 'img/platform.png');
+		game.load.image('star', 'img/star.png');
+		game.load.image('enemy', 'img/enemy.png');
 	},
 	create: function() {
 		// Asset implementaion
@@ -29,29 +47,35 @@ play.prototype = {
 		game.physics.startSystem(Phaser.Physics.ARCADE);
 
 		//ground
-		platform = game.add.group();
-		platform.enableBody = true;
-		var ground = platform.create(0, game.world.height -64, 'platform');
+		this.platform = game.add.group();
+		this.platform.enableBody = true;
+		let ground = this.platform.create(0, game.world.height -64, 'platform');
 		ground.scale.setTo(2,2);
 		ground.body.immovable = true;
 
 		// player
-		player = new Player(game, 'player', 3,3);
+		player = new Player(game, 'player', null, 'star');
 		game.add.existing(player);
 
+		// enemy
+		this.enemy = new Enemy(game, 'enemy', 3, 3);
+		game.add.existing(this.enemy);
 
-		bullets = game.add.group();
-		bullets.enableBody = true;
-		bullets.physicsBodyType = Phaser.Physics.ARCADE;
-		bullets.createMultiple(200, 'star');
+
+		this.bullets = game.add.group();
+		this.bullets.enableBody = true;
+		this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
+		this.bullets.createMultiple(200, 'star');
 		//bullets.setAll('checkWorldBounds', true);
 		//bullets.callAll('events.onOutOfBounds.add', 'events.outOfBounds', resetstar);
-		bullets.checkWorldBounds = true;
-		bullets.outOfBoundsKill = true;
+		this.bullets.checkWorldBounds = true;
+		this.bullets.outOfBoundsKill = true;
 		//bullets.gravity = 300;
-
 		pooCount = 100;
 
+		// sounds
+		deathpoo = game.add.audio('fartdead', 0.6);
+		deathpoo.allowMultiple = false;
 
 
 		// Set camera to platformer follow up
@@ -64,37 +88,54 @@ play.prototype = {
 	},
 	update: function() {
 		// Update function
-		game.physics.arcade.collide(player, platform);
+		// player and enemies collision with platforms
+		game.physics.arcade.collide(player, this.platform);
+		game.physics.arcade.collide(this.enemy, this.platform);
+
+		// enemy movement towards player
+		// if(game.physics.arcade.collide(enemy, platform)){
+		// 	game.physics.arcade.moveToObject(enemy, player);
+		// }
+
+		//shooting
+		/*
 		if(game.input.keyboard.justPressed(Phaser.Keyboard.SPACEBAR)){
-			this.fire();
-		}
-		if(game.physics.arcade.collide(star, platform)){
+			player.fire();
+		}*/
+		if(game.physics.arcade.collide(star, this.platform)){
 			star.kill();
 		}
+		if(pooCount < 0 || pooCount > 100){
+			death();
+		}
 	},
+
 
 	fire: function(){
 		star = bullets.getFirstExists(false);
 		if(star){
+			// gravity for poo
 			game.physics.enable(this, Phaser.Physics.ARCADE);
-			star.body.bounce.y = 0.2;
-			star.body.gravity.y = 10;
+			star.body.bounce.y = 1;
+			star.body.gravity.y = 90;
 			star.body.collideWorldBounds = false;
 			star.reset(player.x + 10, player.y - 10);
-			star.body.velocity.x = 200;
-			//star.gravity = 100;
-			pooCount --;
+			star.body.velocity.x = 250;
 
+			// poo decrement
+			pooCount --;
+			shootpoo = game.add.audio('fart', 0.5);
+			shootpoo.play();
+
+			// checking pooCount
 			console.log(pooCount);
-			if(pooCount < 0 || pooCount > 100){
-				death();
-			}
+			
 		}
 	},
+
 
 	resetstar: function(star){
 		star.kill();
 	}
 	// Char control is implemented in player.js
 }
-
