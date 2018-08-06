50a2034e8556c755e9755d08540bd3134942e455
Sung_Baek <shbaek@ucsc.edu>
2018-08-05 20:32:26 -0700

    starts on menu state now

--- js/state/menu.js
+++ js/state/menu.js
@@ -6,10 +6,17 @@ menu.prototype = {
 	preload: function() {
 		game.load.path = 'assets/';
 		game.load.image('player', 'img/player.png');
-		game.load.image('poo', 'img/star.png');
+		game.load.image('poo', 'img/poo.png');
 		game.load.image('platform', 'img/platform.png');
 		game.load.image('star', 'img/star.png');
 		game.load.image('enemy', 'img/enemy.png');
+		game.load.image('turd', 'img/turd.png');
+		game.load.image('porter', 'img/porter.png');
+
+		//sounds
+		game.load.audio('fart', 'audio/fart.mp3');
+		game.load.audio('rasp', 'audio/Rasp.mp3');
+		game.load.audio('turkey', 'audio/turkey.mp3');
 		// Call menu assets
 	},
 	create: function() {
@@ -29,11 +36,6 @@ menu.prototype = {
 	checkInput: function() {
 		// Input function (menu selction)
 		// Highlight the selected section (or any other indicator to show selected)
-		if (game.input.keyboard.isDown(Phaser.Keyboard.UP)){
-		}
-		if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
-		}
-		if (game.input.keyboard.isDown(Phaser.Keyboard.ENTER)){
-		}
+		
 	}
 }
