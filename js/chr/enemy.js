1731df4597a7561bb49aadf803a45d5b03a9d399
hjung17 <hjung17@ucsc.edu>
2018-08-05 20:42:28 -0700

    enemy ai fixed

--- js/chr/enemy.js
+++ js/chr/enemy.js
@@ -3,19 +3,18 @@ function Enemy(game, key, frame) {
 	// Phaser.Sprite(game, x, y, key)
 	// game.rnd.integerInRange(min, max) returns rand int between min, max
 	Phaser.Sprite.call(this, game, 500, 300, 'enemy', frame);
-	
+	// needs enemy type
+
 	// anchor: Origin of the texture
 	// 0.5 = center
 	this.scale.x = 0.1;
 	this.scale.y = 0.1;
+
 	// physics crap
 	game.physics.enable(this, Phaser.Physics.ARCADE);
 	this.body.bounce.y = 0.2;
 	this.body.gravity.y = 300;
 	this.body.collideWorldBounds = true;
-	// needs enemy type
-
-	game.physics.enable(this);
 }
 
 // explicitly define prefab's prototype (Phaser.Sprite) and constructor
@@ -26,15 +25,11 @@ Enemy.prototype.constructor = Enemy;
 Enemy.prototype.update = function() {
 	game.physics.arcade.collide(this, player, this.pooModifier, null, this);
 	game.physics.arcade.collide(this, player.bullets, this.death, null, this);
+	this.chasePlayer();
 	// trying to get enemy to move towards player when its on a platform
 	// not working, its floating around like a ghost
-	/*
-	if(game.physics.arcade.collide(this, platform)){
-			game.physics.arcade.moveToObject(enemy, player);
-			console.log("moving towards player");
-		}
-		game.physics.arcade.moveToObject(enemy, player);
-*/
+
+	//game.physics.arcade.moveToObject(this, player);
 }
 
 Enemy.prototype.pooModifier = function() {
@@ -58,6 +53,7 @@ Enemy.prototype.death = function(player, bullet) {
 }
 
 Enemy.prototype.chasePlayer = function() {
+	/*
 	// When player is left
 	if (player.body.x < this.body.x) {
 		this.velocity.x = -50;
@@ -65,4 +61,5 @@ Enemy.prototype.chasePlayer = function() {
 	else if (player.body.x > this.body.x) {
 		this.velocity.x = 50;
 	}
+	*/
 }
--- js/state/play.js
+++ js/state/play.js
@@ -3,20 +3,6 @@ var play = function() {
 	this.bullets, this.enemy;
 	this.platform;
 }
-function death(){
-	if(pooCount < 0){
-		player.kill();
-		console.log("death from no poo");
-		player.reset(300,300);
-		pooCount = 10;
-	}
-	if(pooCount > 100){
-		player.kill();
-		console.log("death from too much poo");
-		player.reset(300,300);
-		pooCount = 90;
-	}
-};
 
 play.prototype = {
 	preload: function() {
@@ -57,9 +43,11 @@ play.prototype = {
 		game.add.existing(player);
 
 		// enemy
-		this.enemy = new Enemy(game, 'enemy', 3, 3);
-		game.add.existing(this.enemy);
-
+		this.enemy = game.add.group();
+		this.enemy.enableBody = true;
+		let en = new Enemy(game, 'enemy', null, this.platform);
+		game.add.existing(en);
+		this.enemy.add(en);
 
 		this.bullets = game.add.group();
 		this.bullets.enableBody = true;
@@ -85,7 +73,7 @@ play.prototype = {
 		// Update function
 		// player and enemies collision with platforms
 		game.physics.arcade.collide(player, this.platform);
-		game.physics.arcade.collide(this.enemy, this.platform);
+		game.physics.arcade.collide(this.enemy, this.platform, this.movToPl, null, this);
 
 		// enemy movement towards player
 		// if(game.physics.arcade.collide(enemy, platform)){
@@ -97,12 +85,13 @@ play.prototype = {
 		if(game.input.keyboard.justPressed(Phaser.Keyboard.SPACEBAR)){
 			player.fire();
 		}*/
-		if(game.physics.arcade.collide(star, this.platform)){
-			star.kill();
-		}
+	},
+	movToPl: function(en, platform) {
+		game.physics.arcade.moveToObject(en, player);
 	},
 	render: function() {
 		game.debug.text('Events: ' + player.timer.events, 32, 32, '#ffff00');
+		game.debug.text('Enemy: ' + this.enemy, 32, 64, '#ffff00');
 	}
 	// Char control is implemented in player.js
 }
