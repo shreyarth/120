28210b82b50c0fbcdf9670c4ca523594722839a3
Sung_Baek <shbaek@ucsc.edu>
2018-08-19 15:31:40 -0700

    boss level stuff

--- js/state/boss.js
+++ js/state/boss.js
@@ -1,42 +1,94 @@
-var boss = function() {
+var play = function() {
 	// Global state variables
 	this.bullets, this.enemy;
 	this.platform; this.en3;
+	this.obstacle;
 	this.heller = null;
 	this.ui;
 }
 
-boss.prototype = {
+play.prototype = {
 	preload: function() {
 		
 	},
 	create: function() {
 		// Asset implementaion
-		game.world.setBounds( 0, 0, 1000, 1200);
+		game.world.setBounds( 0, 0, 800, 800);
 		console.log("play state to check implementation");
+		game.physics.startSystem(Phaser.Physics.P2jS);
 		
 		var background = game.add.sprite(0, 0, 'porter');
 		//background.scale.setTo(5,2);
 		background.width = game.world.width;
-		background.height = 800;
+		background.height = game.world.height -32;
+		// this.heller = this.add.tileSprite(0, game.world.height - 500, game.world.width, 1000, 'heller');
+		// this.heller.height = 800;
 
 		//ground
 		this.platform = game.add.group();
 		this.platform.enableBody = true;
 		let ground = this.platform.create(0, game.world.height -32, 'platform');
-		ground.scale.setTo(2,1);
+		ground.scale.setTo(game.world.width, 1 );
 		ground.body.immovable = true;
+		
+		//platforms in order, left to right
+		
+		let platforms = this.platform.create(400, 890, 'bus');
+		platforms.body.immovable = true;
+		platforms.scale.setTo(1,1);
+
+		platforms = this.platform.create(1000, 1040, 'rcar');
+		platforms.body.immovable = true;
+
+		platforms.scale.setTo(0.5,0.5);
+
+		platforms = this.platform.create(2900, 700, 'bus');
+		platforms.body.immovable = true;
+		platforms.anchor.setTo(0.5, 0.5);
+		platforms.angle = 180;
+		platforms.scale.setTo(1,1);
+
+		platforms = this.platform.create(2400, 1040, 'ycar');
+		platforms.body.immovable = true;
+		platforms.scale.setTo(0.5,0.5);
+		platforms = this.platform.create(2960, 1040, 'rcar');
+		platforms.body.immovable = true;
+		platforms.scale.setTo(0.5,0.5);
+
+		platforms = this.platform.create(1600, 1040, 'ycar');
+		platforms.body.immovable = true;
+		platforms.scale.setTo(0.5,0.5);
+
+		//double verticle bus
+		platforms = this.platform.create(1850, 630, 'busObs');
+		platforms.body.immovable = true;
+		platforms.scale.setTo(0.35,0.35);
+
+
+		//stack of car and bus
+		platforms = this.platform.create(3300, 670, 'carObs');
+		platforms.body.immovable = true;
+		platforms.scale.setTo(0.4,0.4);
+
+		//crashed cars and bus in buildings
+		platforms = this.platform.create(1680, 420, 'wreckC');
+		platforms.body.immovable = true;
+		platforms.scale.setTo(0.6,0.6); 
+
+		platforms = this.platform.create(2530, 300, 'wreckB');
+		platforms.body.immovable = true;
+		platforms.scale.setTo(1,1);
 
 		// the background wrap
 		// var wrapGround = game.add.sprite(0, game.world.height - 300, 'heller');
 		// wrapGround.scale.setTo(2,0.8);
 		// wrapGround.width = game.width;
-		this.heller = this.add.tileSprite(0, game.world.height - 500, game.world.width, game.height/2, 'heller');
+		// this.heller = this.add.tileSprite(0, game.world.height - 500, game.world.width, game.height/2, 'heller');
 
-		game.physics.startSystem(Phaser.Physics.ARCADE);
+		game.physics.startSystem(Phaser.Physics.P2JS);
 
 		// player
-		player = new Player(game, 'player', null, 'poo');
+		player = new P2layer(game, 'player', null, 'poo');
 		game.add.existing(player);
 
 		//camera
@@ -46,29 +98,49 @@ boss.prototype = {
 		// enemy
 		this.enemy = game.add.group();
 		this.enemy.enableBody = true;
-		let en = new Enemy(game, 'enemy', null, this.platform);
-		game.add.existing(en);
-		this.enemy.add(en);
+		
+		for(var i = 0; i < 30; ++i){
+			let en = new Enemy(game, game.rnd.integerInRange(600,4900),
+				game.rnd.integerInRange(200,1000), 'enemy');
+			game.add.existing(en);
+			this.enemy.add(en);
+		}
 
 		//test for 2nd enemy on screen
-		let en2 = new Enemy(game, 'enemy', null);
-		game.add.existing(en2);
-		this.enemy.add(en2);
+		// en = new Enemy(game, 30, 1000, 'enemy');
+		// game.add.existing(en);
+		// this.enemy.add(en);
 
 		//test for flying enemy
-		this.en3 = new Enemy(game, 'enemy');
-		game.add.existing(this.en3);
-		this.en3.body.reset(30,400);
+		for(var i = 0; i < 10; ++i){
+			en = new Enemy(game, game.rnd.integerInRange(1000,4800),
+			 400, 'enemy');
+			game.add.existing(en);
+			this.enemy.add(en);
+		}
 
 		this.bullets = game.add.group();
 		this.bullets.enableBody = true;
-		this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
-		this.bullets.createMultiple(200, 'star');
+		this.bullets.physicsBodyType = Phaser.Physics.p2;
+		//this.bullets.createMultiple(200, 'star');
 		//bullets.setAll('checkWorldBounds', true);
 		//bullets.callAll('events.onOutOfBounds.add', 'events.outOfBounds', resetstar);
 		this.bullets.checkWorldBounds = true;
 		this.bullets.outOfBoundsKill = true;
 		//bullets.gravity = 300;
+
+		//enemies bullets
+		this.bulletE = game.add.group();
+		this.bulletE.enableBody = true;
+		this.bullets.physicsBodyType = Phaser.Physics.p2;
+		this.bullets.createMultiple(200, 'star');
+		this.bullets.checkWorldBounds = true;
+		this.bullets.outOfBoundsKill = true;
+
+		//sign for end of level
+		let sign = this.platform.create(4800, 900, 'sign');
+		sign.body.immovable = true;
+		sign.scale.setTo(1,1);
 
 		//pooCount = 100;
 
@@ -78,20 +150,20 @@ boss.prototype = {
 
 		// Fix UI to the camera
 		this.ui = this.pooMeter(player.pooCount);
-		this.ui.fixedToCamera = true;
 	},
 	pooMeter: function(pooNum) {
 		let obj = null;
 
 		// create primitive
 		let g = game.add.graphics();
-		g.beginFill(0x00FF00);
-		g.drawRect(32, 32, pooNum * 10, 32);	// Starting point, width, height
+		g.beginFill(0x492008);
+		g.drawRect(32, 32, pooNum * 5, 32);	// Starting point, width, height
 		g.endFill();
 
 		// transform primitive into sprite and destroy primitive
 		obj = game.add.sprite(32, 32, g.generateTexture());
-		//obj.anchor.set(0.5, 0.5);
+		obj.fixedToCamera = true;
+		obj.cameraOffset.setTo(32, 16);
 		g.destroy();
 
 		return obj;
@@ -102,19 +174,19 @@ boss.prototype = {
 		game.physics.arcade.collide(player, this.platform);
 		game.physics.arcade.collide(this.enemy, this.platform, this.movToPl, null, this);
 
-		if(player.body.velocity.x == 0){
-			this.heller.tilePosition.x = this.heller.tilePosition.x;
-		}else if(player.body.velocity.x > 0){
-			this.heller.tilePosition.x -= 4;
-		}else{
-			this.heller.tilePosition.x += 4;
-		}
+		// if(player.body.velocity.x == 0){
+		// 	this.heller.tilePosition.x = this.heller.tilePosition.x;
+		// }else if(player.body.velocity.x > 0){
+		// 	this.heller.tilePosition.x -= 4;
+		// }else{
+		// 	this.heller.tilePosition.x += 4;
+		// }
 		
 		// enemy movement towards player
 		// if(game.physics.arcade.collide(enemy, platform)){
 		// 	game.physics.arcade.moveToObject(enemy, player);
 		// }
-		game.physics.arcade.moveToObject(this.en3, player);
+		//game.physics.arcade.moveToObject(this.en3, player);
 
 		// UI update
 		this.ui.destroy();
@@ -125,10 +197,13 @@ boss.prototype = {
 		if(game.input.keyboard.justPressed(Phaser.Keyboard.SPACEBAR)){
 			player.fire();
 		}*/
+		//for end of level
+		if(player.x +30 > game.world.width){
+			game.state.start('end');
+		}
 	},
 	movToPl: function(en, platform) {
 		game.physics.arcade.moveToObject(en, player);
 	}
 	// Char control is implemented in player.js
-
 }
