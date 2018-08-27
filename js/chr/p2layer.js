bdec16398fcfc72090069a6a72771c9b710dfe57
Shreyarth <shbhatt@ucsc.edu>
2018-08-26 19:58:05 -0700

    Minor changes to p2layer hitbox

--- js/chr/p2layer.js
+++ js/chr/p2layer.js
@@ -32,7 +32,7 @@ function P2layer(game, key, frame, bulletKey) {
 	this.game.physics.p2.gravity.y = 100;
 	this.body.data.gravityScale = 2;
 	//this.body.mass = 200;
-	this.body.setRectangle(this.width -10, this.height -12);
+	this.body.setRectangle(this.width -25, this.height);
 	this.pDown = true;
 	this.state = 'idle';
 	this.direction = 'right';
